7fcbcf03aacfe51e18b206de28996dd44a559ea2
Daniel Velásquez Méndez <develasmen@gmail.com>
2025-02-07 16:38:07 -0600

    Semana 3 - Receso

--- View/Login/recuperarContrasenna.php
+++ View/Login/recuperarContrasenna.php
@@ -1,7 +1,3 @@
-<?php
-    include_once $_SERVER["DOCUMENT_ROOT"] . "/Proyecto/Controller/LoginController.php";
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -25,43 +21,36 @@
 
     <div class="container MargenSuperior">
 
+
         <div class="row justify-content-center">
 
             <div class="col-xl-10 col-lg-12 col-md-9">
 
                 <div class="card o-hidden border-0 shadow-lg my-5">
-                    <div class="card-body p-0">
 
                         <div class="row">
                             <div class="col-lg-3"></div>
                             <div class="col-lg-6">
                                 <div class="p-5">
                                     <div class="text-center">
-                                        <h1 class="h4 text-gray-900 mb-4">Proyecto MN</h1>
+                                        <h1 class="h4 text-gray-900 mb-4">Recuperar Contrasena</h1>
+                                        <p class="mb-4">We get it, stuff happens. Just enter your email address below
+                                            and we'll send you a link to reset your password!</p>
                                     </div>
-
-                                    <form action="" method="POST" class="user">
+                                    <form class="user">
                                         <div class="form-group">
-                                            <input type="text" class="form-control form-control-user"
-                                                id="txtIdentificacion" name="txtIdentificacion"
-                                                placeholder="Identificación">
-                                        </div>
-                                        <div class="form-group">
-                                            <input type="password" class="form-control form-control-user"
-                                                id="txtContrasenna" name="txtContrasenna" 
-                                                placeholder="Contraseña">
+                                            <input type="email" class="form-control form-control-user" 
+                                            placeholder="Correo" id="txtCorreo" name="txtCorreo" >
                                         </div>
                                         <input type="submit" class="btn btn-danger btn-user btn-block" value="Procesar"
-                                        id="btnIniciarSesion" name="btnIniciarSesion" >
+                                        id="btnRecuperarCuenta" name="btnRecuperarCuenta" >
                                     </form>
-
                                     <hr>
-                                    
-                                    <div class="text-center">
-                                        <a class="small" href="recuperarContrasenna.php">Recuperar Contraseña</a>
-                                    </div>
                                     <div class="text-center">
                                         <a class="small" href="registrarCuenta.php">Crear una Cuenta</a>
+                                    </div>
+                                    <div class="text-center">
+                                        <a class="small" href="login.php">Iniciar Sesion</a>
                                     </div>
                                 </div>
                             </div>
